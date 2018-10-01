038d6f47383f387da6cef2c7379dea89e2b6ed8f
Alan <alan15-7@hotmail.com>
2018-10-01 16:30:02 +0200

    Set-up project

---
+++ app/src/main/java/com/twelker/geo_guess_swipe/GeoGuessSwipe.java
@@ -0,0 +1,13 @@
+package com.twelker.geo_guess_swipe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GeoGuessSwipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_geo_guess_swipe);
+    }
+}
